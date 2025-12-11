adddb9632b343ebbfb0cb812491ce423882c43a9
srhodes0856-web <srhodes0856@sdsu.edu>
2025-12-10 21:35:57 -0800

    Clean up diamond mining loop in diamond_mcraft.asm
    Removed commented-out code for clarity and streamlined the diamond mining loop.

--- exampleprograms/diamond_mcraft.asm
+++ exampleprograms/diamond_mcraft.asm
@@ -16,9 +16,6 @@ main:
         ADDI    $t2, $zero, 0 # R2 = 0 (diamond count)
 
         # Assume R3 ($t3) is DIAMOND ID and R5 ($t5) is starting WORLD index
-        # e.g., you could do:
-        # ADDI  $t3, $zero, 42 # DIAMOND ID = 42
-        # ADDI  $t5, $zero, 0
 
 diamond_loop:
         MINE    $t4, 0($t5) # R4 = WORLD[R5]
@@ -30,7 +27,6 @@ no_diamond:
         ADDI    $t5, $t5, 1 # advance WORLD pointer
 
         # SPRINT R1, diamond_loop
-        # Our SPRINT decrements R1 and branches while R1 != 0
         SPRINT  $t1, diamond_loop
 
 done:
