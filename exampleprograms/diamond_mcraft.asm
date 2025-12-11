32cd3b5f5de61d8d123dabb8777dce229ee8326b
srhodes0856-web <srhodes0856@sdsu.edu>
2025-12-10 21:33:14 -0800

    Refactor diamond_mcraft.asm for consistency

--- exampleprograms/diamond_mcraft.asm
+++ exampleprograms/diamond_mcraft.asm
@@ -12,27 +12,27 @@ main:
         # R4 -> $t4 (temp block from WORLD)
         # R5 -> $t5 (block pointer in WORLD)
 
-        ADDI    $t1, $zero, 10    # R1 = 10 cells to check
-        ADDI    $t2, $zero, 0     # R2 = 0 (diamond count)
+        ADDI    $t1, $zero, 10 # R1 = 10 cells to check
+        ADDI    $t2, $zero, 0 # R2 = 0 (diamond count)
 
         # Assume R3 ($t3) is DIAMOND ID and R5 ($t5) is starting WORLD index
         # e.g., you could do:
-        # ADDI  $t3, $zero, 42     # DIAMOND ID = 42
+        # ADDI  $t3, $zero, 42 # DIAMOND ID = 42
         # ADDI  $t5, $zero, 0
 
 diamond_loop:
-        MINE    $t4, 0($t5)       # R4 = WORLD[R5]
+        MINE    $t4, 0($t5) # R4 = WORLD[R5]
 
         BNE     $t4, $t3, no_diamond
-        ADDI    $t2, $t2, 1       # if block == DIAMOND, count++
+        ADDI    $t2, $t2, 1 # if block == DIAMOND, count++
 
 no_diamond:
-        ADDI    $t5, $t5, 1       # advance WORLD pointer
+        ADDI    $t5, $t5, 1 # advance WORLD pointer
 
         # SPRINT R1, diamond_loop
         # Our SPRINT decrements R1 and branches while R1 != 0
         SPRINT  $t1, diamond_loop
 
 done:
-        CHAT    $t2               # print diamond count (per your spec)
+        CHAT    $t2
         ADD     $zero, $zero, $zero
